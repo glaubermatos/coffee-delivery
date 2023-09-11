3d7e6799376ef6ed7129dea7afb2d10d260bdecb
glaubermatos <glaub.oliveira@hotmail.com>
2023-09-11 17:35:39 -0300

    add phosphoricons

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,11 @@
+import { Horse } from 'phosphor-react-native';
+
 import { Container, Text, Text2 } from "./styles";
 
 export function Home() {
     return (
         <Container>
+            <Horse color='white' size={32} />
             <Text>Teste</Text>
             <Text2>Home screen</Text2>
         </Container>
