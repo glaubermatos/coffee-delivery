68c6995347f07576a092c7953e75f3b98c699583
glaubermatos <glaub.oliveira@hotmail.com>
2023-09-12 11:24:05 -0300

    ilustrations with react-native-svg-transformer

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,17 @@
 import { Horse } from 'phosphor-react-native';
 
+import ScooterImg from '@assets/scooter.svg'
+
 import { Container, Text, Text2 } from "./styles";
 
 export function Home() {
     return (
         <Container>
+            <ScooterImg 
+                width={224}
+                height={288}
+            />
+            
             <Horse color='white' size={32} />
             <Text>Teste</Text>
             <Text2>Home screen</Text2>
